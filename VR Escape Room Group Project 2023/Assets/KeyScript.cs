4b8bd76fd292c36a360617fc15ed373c39c3273e
SharpEye125 <creeperminer125@gmail.com>
2023-01-05 12:02:09 -0800

    Testing Key Script

--- VR Escape Room Group Project 2023/Assets/KeyScript.cs
+++ VR Escape Room Group Project 2023/Assets/KeyScript.cs
@@ -5,6 +5,8 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class KeyScript : MonoBehaviour
 {
+    bool inKeySlot = false;
+    GameObject prefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +19,25 @@ public class KeyScript : MonoBehaviour
     {
 
     }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Key Slot"))
+        {
+            inKeySlot = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Key Slot"))
+        {
+            inKeySlot = false;
+        }
+    }
     public void KeyActivate(ActivateEventArgs arg)
     {
-
+        if (inKeySlot == true)
+        {
+            Instantiate(prefab, transform.position + new Vector3(0, 5, 0), transform.rotation.normalized);
+        }
     }
 }
