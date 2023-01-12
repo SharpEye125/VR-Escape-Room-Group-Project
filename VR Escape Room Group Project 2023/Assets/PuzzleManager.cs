f66da34bb3d5d6541aaf2113911d35345e031204
Cel-Johnson <92333735+Cel-Johnson@users.noreply.github.com>
2023-01-12 13:27:02 -0800

    not broken?

--- VR Escape Room Group Project 2023/Assets/PuzzleManager.cs
+++ VR Escape Room Group Project 2023/Assets/PuzzleManager.cs
@@ -6,11 +6,47 @@ public class PuzzleManager : MonoBehaviour
 {
     public int stage;
 
-    public GameObject button;
-    public Vector3 buttonAL;
-    public Vector3 buttonIL;
+    public GameObject b;
+    public Vector3 bAL;
+    public Vector3 bIL;
 
-    public GameObject 
+    public GameObject d;
+    public Vector3 dAL;
+    public Vector3 dIL;
+
+    public GameObject kH1;
+    public Vector3 kH1AL;
+    public Vector3 kH1IL;
+
+    public GameObject p3x3;
+    public Vector3 p3x3AL;
+    public Vector3 p3x3IL;
+
+    public GameObject bP;
+    public Vector3 bPlAL;
+    public Vector3 bPlIL;
+
+    public GameObject p2x2;
+    public Vector3 p2x2AL;
+    public Vector3 p2x2IL;
+
+    public GameObject kH2;
+    public Vector3 kH2AL;
+    public Vector3 kHr2IL;
+
+    public GameObject kH3;
+    public Vector3 kH3AL;
+    public Vector3 kH3IL;
+
+    public GameObject kH4;
+    public Vector3 kH4AL;
+    public Vector3 kH4IL;
+
+    public GameObject key1;
+    public GameObject key2;
+    public GameObject key3;
+    public GameObject key4;
+
     /*
      This is a list of the stages and what part of the puzzle it refers to.
 
@@ -24,15 +60,56 @@ public class PuzzleManager : MonoBehaviour
         -Stage 7: Key slot 3 is complete so key slot 4 appears                                                                                                                                7    Key 3 done
         -Stage 8: Key slot 4 is complete so all keys and key slots receed and some victory stuff happens.                                                                                       8    Key 4 done, all is done
     */
-  
+
     void Start()
-    {
-        
+    { 
+      
     }
+      
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(stage ==0)
+        {
+
+        }
+        else
+        {
+
+        }
+        if (stage == 1)
+        {
+
+        }
+        else
+        {
+
+        }
+        if (stage == 2)
+        {
+
+        }
+        else
+        {
+
+        }
+        if (stage == 3)
+        {
+
+        }
+        else
+        {
+
+        }
+        if (stage == 0)
+        {
+
+        }
+        else
+        {
+
+        }
     }
 }
