e3fa60e7d9748730a9316adadfe5e3dc095ef51c
LeviTerrell5 <92333687+LeviTerrell5@users.noreply.github.com>
2023-01-13 13:13:09 -0800

    huh

---
+++ VR Escape Room Group Project 2023/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Timer Components")]
+    [SerializeField] private float gameTime;
+    [SerializeField] Text timeTextBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateGameTimer();
+    }
+
+    private void UpdateGameTimer()
+    {
+        gameTime -= Time.deltaTime;
+
+        var minutes = Mathf.FloorToInt(gameTime / 60);
+        var seconds = Mathf.FloorToInt(gameTime - minutes * 60);
+
+        string gameTimeClockDisplay = string.Format()
+    }
+}
