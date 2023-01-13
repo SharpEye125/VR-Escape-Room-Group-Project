7183afa353ee0036b62c46a8e83ed3ef7cdb8f9d
SharpEye125 <creeperminer125@gmail.com>
2023-01-13 12:54:16 -0800

    Script made to let go of objects that are no longer in grab range

---
+++ VR Escape Room Group Project 2023/Assets/Scripts/VRDistanceForceLetGo.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class VRDistanceForceLetGo : MonoBehaviour
+{
+    public XRDirectInteractor myHandInteractor;
+    public XRRayInteractor myRayInteractor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myHandInteractor = GetComponent<XRDirectInteractor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 7)
+        {
+            myHandInteractor.allowSelect = true;
+            myRayInteractor.allowSelect = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 7)
+        {
+            myHandInteractor.allowSelect = false;
+            myRayInteractor.allowSelect = false;
+        }
+    }
+}
